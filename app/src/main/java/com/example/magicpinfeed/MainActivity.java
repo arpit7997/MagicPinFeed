f95bc3dd6c30643ffabb118de6e32d8b4dc1f904
Arpit Gupta <guptaarpit030@gmail.com>
2019-03-04 16:05:18 +0530

    added dynamic thumbnail
    Signed-off-by: Arpit Gupta <guptaarpit030@gmail.com>

--- app/src/main/java/com/example/magicpinfeed/MainActivity.java
+++ app/src/main/java/com/example/magicpinfeed/MainActivity.java
@@ -87,11 +87,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void initFeedArrayList() {
 
-        feedArrayList.add(new Feed("How's the Josh at Magic Pin? ", "", "https://player.vimeo.com/external/286837529.m3u8?s=20f83af6ea8fbfc8ce0c2001f32bf037f8b0f65f"));
-        feedArrayList.add(new Feed("Introduction to  Magic Pin", "", "https://player.vimeo.com/external/286837767.m3u8?s=42570e8c4a91b98cdec7e7bfdf0eccf54e700b69"));
-        feedArrayList.add(new Feed("Why choose Magic Pin", "", "https://player.vimeo.com/external/286837810.m3u8?s=610b4fee49a71c2dbf22c01752372ff1c6459b9e"));
-        feedArrayList.add(new Feed("Meet the Team", "", "https://player.vimeo.com/external/286837723.m3u8?s=3df60d3c1c6c7a11df4047af99c5e05cc2e7ae96"));
-        feedArrayList.add(new Feed("Featured in Hindustan Times", "", "https://player.vimeo.com/external/286837649.m3u8?s=9e486e9b932be72a8875afc6eaae21bab124a35a"));
-        feedArrayList.add(new Feed("Last Video", "", "https://player.vimeo.com/external/286837402.m3u8?s=7e01c398e2f01c29ecbd46e5e2dd53e0d6c1905d"));
+        feedArrayList.add(new Feed("How's the Josh at Magic Pin? ", "https://firebasestorage.googleapis.com/v0/b/urban-clap-copy.appspot.com/o/IMG_20190304_155617.jpg?alt=media&token=cc416e05-3ce9-4bab-ae21-25a1be8bedad", "https://player.vimeo.com/external/286837529.m3u8?s=20f83af6ea8fbfc8ce0c2001f32bf037f8b0f65f"));
+        feedArrayList.add(new Feed("Introduction to  Magic Pin", "https://firebasestorage.googleapis.com/v0/b/urban-clap-copy.appspot.com/o/IMG_20190304_155604.jpg?alt=media&token=da823bda-3ab1-40e7-83ba-4412c222e022", "https://player.vimeo.com/external/286837767.m3u8?s=42570e8c4a91b98cdec7e7bfdf0eccf54e700b69"));
+        feedArrayList.add(new Feed("Why choose Magic Pin", "https://firebasestorage.googleapis.com/v0/b/urban-clap-copy.appspot.com/o/IMG_20190304_155524.jpg?alt=media&token=cfcaa22f-9313-4590-af23-537ad126784f", "https://player.vimeo.com/external/286837810.m3u8?s=610b4fee49a71c2dbf22c01752372ff1c6459b9e"));
+        feedArrayList.add(new Feed("Meet the Team", "https://firebasestorage.googleapis.com/v0/b/urban-clap-copy.appspot.com/o/IMG_20190304_155524.jpg?alt=media&token=cfcaa22f-9313-4590-af23-537ad126784f", "https://player.vimeo.com/external/286837723.m3u8?s=3df60d3c1c6c7a11df4047af99c5e05cc2e7ae96"));
+        feedArrayList.add(new Feed("Featured in Hindustan Times", "https://firebasestorage.googleapis.com/v0/b/urban-clap-copy.appspot.com/o/IMG_20190304_155509.jpg?alt=media&token=7f1343cc-077e-494e-b050-2cb2afe21f35", "https://player.vimeo.com/external/286837649.m3u8?s=9e486e9b932be72a8875afc6eaae21bab124a35a"));
+        feedArrayList.add(new Feed("Last Video", "https://firebasestorage.googleapis.com/v0/b/urban-clap-copy.appspot.com/o/IMG_20190304_155630.jpg?alt=media&token=56a88443-f6c9-43ed-9f2a-28820f2924d7", "https://player.vimeo.com/external/286837402.m3u8?s=7e01c398e2f01c29ecbd46e5e2dd53e0d6c1905d"));
     }
 }
